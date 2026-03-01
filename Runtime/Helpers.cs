7afcc66614a469140b5d08f33a505e4fa0893465
sobia98 <96776908+sobia98@users.noreply.github.com>
2026-03-01 16:56:27 +0100

    Initial commit

---
+++ Runtime/Helpers.cs
@@ -0,0 +1,10 @@
+namespace Sobia.Utils
+{
+    public static class PrototypeUtils
+    {
+        public static void QuickLog(string message)
+        {
+            UnityEngine.Debug.Log($"[Prototype] {message}");
+        }
+    }
+}

89506b873add583cfbb32dc801eb2ff035b98cc1
sobia98 <96776908+sobia98@users.noreply.github.com>
2026-05-26 17:45:00 +0200

    event for getting money way easier

--- Runtime/Global/GameEvents.cs
+++ Runtime/Global/GameEvents.cs
@@ -1,7 +1,10 @@
 public static class GameEvents
 {
     public static System.Action<bool> OnTogglePause;
+    public static System.Action<bool> OnToggleTab;
     public static System.Action OnFOVChanged;
+    public static System.Action<float, string> OnObjectPaintingCompleted;
+    public static System.Action<float> OnCurrentMoneyChanged;
 
     public static void TriggerTogglePause(bool isPaused)
     {
@@ -11,5 +14,20 @@ public static class GameEvents
     public static void TriggerFOVChanged()
     {
         OnFOVChanged?.Invoke();
+    }
+
+    public static void TriggerToggleTab(bool isTabOpen)
+    {
+        OnToggleTab?.Invoke(isTabOpen);
+    }
+
+    public static void TriggerObjectPaintingCompleted(float cashReward, string rewardStatID)
+    {
+        OnObjectPaintingCompleted?.Invoke(cashReward, rewardStatID);
+    }
+
+    public static void TriggerCurrentMoneyChanged(float newAmount)
+    {
+        OnCurrentMoneyChanged?.Invoke(newAmount);
     }
 }
